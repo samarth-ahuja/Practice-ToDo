d677da7448dae82736d8358af1d4ae39ee043894
Samarth Ahuja <samarth.ahuja@unthinkable.co>
2024-06-22 21:00:40 +0530

    trying strict mode after deploying

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -11,18 +11,18 @@ function taskListReducer(state, action) {
             id: Math.floor(Math.random() * 10000),
             completed: false
         };
-        action.setLogs(prev=>[...prev,{id:Math.floor(Math.random()*1000000),log:'Added a task with ' + newTask.id}]);
+        action.log({id:Math.floor(Math.random()*1000000),log:'Added a task with ' + newTask.id});
         return [...state, newTask];
     }
     if (action.type === 'REMOVE_TASK') {
-        action.setLogs(prev=>[...prev,{id:Math.floor(Math.random()*1000000),log:'Deleted a task with ' + action.payload.item.id}]);
+        action.log({id:Math.floor(Math.random()*1000000),log:'Deleted a task with ' + action.payload.item.id});
         return state.filter(listItem => listItem.id != action.payload.item.id)
     }
     if (action.type === 'STATE_CHANGE') {
         let newList = [];
         for (let x of state) {
             if (x.id === action.payload.item.id) {
-                action.setLogs(prev=>[...prev,{id:Math.floor(Math.random()*1000000),log:'Task with id ' + x.id + ' is ' + (!x.completed ? 'completed.' : 'pending')}]);
+                action.log({id:Math.floor(Math.random()*1000000),log:'Task with id ' + x.id + ' is ' + (!x.completed ? 'completed.' : 'pending')});
                 newList.push({ ...x, completed: !x.completed })
             }
             else {
@@ -37,13 +37,17 @@ let logStatements = [];
 export default function TodoApp() {
     const [taskList, taskListDispatcher] = useReducer(taskListReducer, []);
     const [logState,setLogState] = useState([]);
+    function handleLog(log){
+        console.log('Logging:', log);
+        setLogState(prev=>[...prev,log])
+    }
     function newTaskHandler(text) {
         taskListDispatcher({
             type: 'ADD_TASK',
             payload: {
                 text: text,
             },
-            setLogs:setLogState
+            log:handleLog
         })
     }
     function deleteHandler(item) {
@@ -52,7 +56,7 @@ export default function TodoApp() {
             payload: {
                 item: item,
             },
-            setLogs:setLogState
+            log:handleLog
         })
     }
     function stateChangeHandler(item) {
@@ -61,7 +65,7 @@ export default function TodoApp() {
             payload: {
                 item: item,
             },
-            setLogs:setLogState
+            log:handleLog
         })
     }
     return (
